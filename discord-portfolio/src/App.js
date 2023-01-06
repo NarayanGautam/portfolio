37291842a28b8bde3f8b5cd9bdb05a42e1425da6
Shuva Gautam <gautns7@gmail.com>
2023-01-06 01:12:24 -0500

    added topnav, dark mode effect

--- discord-portfolio/src/App.js
+++ discord-portfolio/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import SideBar from './components/SideBar';
+import TopNav from './components/TopNav'
 
 function App() {
   return (
     <div className='flex'>
       <SideBar/>
+      <TopNav/>
     </div>
   );
 }
--- discord-portfolio/src/components/TopNav.jsx
+++ discord-portfolio/src/components/TopNav.jsx
@@ -23,3 +23,5 @@ const ThemeIcon = () => {
     </span>
     )
 }
+
+export default TopNav
