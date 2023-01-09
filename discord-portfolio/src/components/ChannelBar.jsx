908b897ebe4013f442a61262b510564048c8c0ac
Shuva Gautam <gautns7@gmail.com>
2023-01-09 11:09:21 -0500

    added base channel bar and content container

---
+++ discord-portfolio/src/components/ChannelBar.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+
+// figure out how to get this data based on what page they are on 
+const topics = {};
+
+const ChannelBar = () => {
+    return (
+        <div className="channel-bar">
+            
+        </div>
+    )
+}
+
+
+
--- discord-portfolio/src/components/Content.jsx
+++ discord-portfolio/src/components/Content.jsx
@@ -1,1 +1,12 @@
 import TopNav from "./TopNav";
+
+const Content = () => {
+    return (
+        <div className="content">
+            <TopNav />
+            <div className="content-list">
+                
+            </div>
+        </div>
+    )
+}
