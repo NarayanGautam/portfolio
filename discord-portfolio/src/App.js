29f3c10023dcd74b753ca070e0c6c955aa8a77f1
Shuva Gautam <gautns7@gmail.com>
2023-01-09 21:48:39 -0500

    finished static design

--- discord-portfolio/src/App.js
+++ discord-portfolio/src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
+import ChannelBar from './components/ChannelBar';
 import SideBar from './components/SideBar';
-import TopNav from './components/TopNav'
+import Content from './components/Content';
 
 function App() {
   return (
     <div className='flex'>
       <SideBar/>
-      <TopNav/>
+      <ChannelBar/>
+      <Content/>
     </div>
   );
 }
--- discord-portfolio/src/components/Content.jsx
+++ discord-portfolio/src/components/Content.jsx
@@ -5,8 +5,38 @@ const Content = () => {
         <div className="content">
             <TopNav />
             <div className="content-list">
-                
+                <Post 
+                    name = "Tailwind CSS"
+                    time = "2 hours ago"
+                    text = {"Lorem ipsum dolor sit amet, consectetur adipiscing elit."}
+                />
+                <Post 
+                    name = "Tailwind CSS"
+                    time = "2 hours ago"
+                    text = {"Lorem ipsum dolor sit amet, consectetur adipiscing elit."}
+                />
             </div>
         </div>
     )
 }
+
+const Post = ({ name, time, text }) => (
+    <div className="post">
+        <div className="avatar-wrap">
+            <img src="https://avatars.dicebear.com/api/open-peeps/1.svg" alt="avatar" />
+        </div>
+        <div className="post-content">
+            <div className="post-writer">
+                {name}
+                <small className="post-time">
+                    {time}
+                </small>
+            </div>
+            <div className="post-text">
+                {text}
+            </div>
+        </div>
+    </div>
+)
+
+export default Content;
