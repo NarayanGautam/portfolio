26fb3544230dd5c3be1e4e75cea8e6db7d3fb70b
Shuva Gautam <gautns7@gmail.com>
2023-01-18 03:14:20 -0500

    added custom data to content container

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -3,67 +3,57 @@ import icon from "../assets/profile_icon.png";
 import { useState } from "react";
 
 const discord_icon = "https://cdn.logojoy.com/wp-content/uploads/20210422095037/discord-mascot.png";
+
 const Content = (data) => {
-    //const [content_data, setContent] = useState(data.about);
-    let content_data = data.channels;
-    console.log(content_data);
+    const [content_data, setContent] = useState(data.skills);
+    const [time, setTime] = useState(Math.floor(Math.random() * 30 + 30));
+    
     return (
         <div className="content">
             <TopNav />
+            <div className="content-list">
             {Object.keys(content_data).map((key) => (
-                <div className="content-list">
-                    <Post 
-                        name = "viewr"
-                        time = "2 hours ago"
-                        text = {key}
-                        img = {discord_icon}
+                //setTime(time-2),
+                <>
+                <Post 
+                    name = "Viewer"
+                    msgtime = {time + " minutes ago"}
+                    text = {key}
+                    img = {discord_icon}
+                />
+                <Post 
+                    name = "Shuva Gautam"
+                    msgtime = {time -2 + " minutes ago"}
+                    text = {content_data[key]}
+                    img = {icon}
                     />
-                    <Post 
-                        name = "Shuva Gautam"
-                        time = "2 hours ago"
-                        text = {content_data[key]}
-                        img = {icon}
-                    />
-                </div>
-            ))}
+                </>
+                ))}
+            </div>
         </div>
-        
-
-            /*<div className="content">
-                <TopNav />
-                <div className="content-list">
-                    <Post 
-                        name = "Shuva Gautam"
-                        time = "2 hours ago"
-                        text = {"Lorem ipsum dolor sit amet, consectetur adipiscing elit."}
-                    />
-                    <Post 
-                        name = "Tailwind CSS"
-                        time = "2 hours ago"
-                        text = {"Lorem ipsum dolor sit amet, consectetur adipiscing elit."}
-                    />
-                </div>
-            </div> */
     )
 }
 
-const Post = ({ name, time, text, img }) => (
-    <div className="post">
-        <div className="avatar-wrap">
-            <img src={img} alt="avatar" className="avatar" />
-        </div>
-        <div className="post-content">
-            <div className="post-writer">
-                {name}
-                <small className="post-time">
-                    {time}
-                </small>
+
+const Post = ({ name, msgtime, text, img }) => (
+    text.split(/[./!]/).map((sentence) => (
+        <div className="post">
+            <div className="avatar-wrap">
+                <img src={img} alt="avatar" className="avatar" />
             </div>
-            <div className="post-text">
-                {text}
+            <div className="post-content">
+                <div className="post-writer">
+                    {name}
+                    <small className="post-time">
+                        {msgtime}
+                    </small>
+                </div>
+                <div className="post-text">
+                    {sentence}
+                </div>
             </div>
         </div>
-    </div>
+    ))
 )
 
-export default Content;
+export default Content
--- src/pages/Experience.jsx
+++ src/pages/Experience.jsx
@@ -10,11 +10,26 @@ export default function Experience() {
     }
   }
 
+  let content_data = {
+    /*"channels": {
+      "General": [ "about me", "my skills", "resume", "contact me"],
+      "Projects": ["project 1", "project 2", "project 3", "project 4"],
+    } */
+     "about": {
+      "Who are you?": "My name is Shuva Narayan Gautam living in Ottawa. I am a student at Carleton University pursuing a Bachelor of Computer Science. I am a an aspiring software developer and I am passionate about learning new technologies and applying them to solve real-world problems. I am currently looking for a co-op position for Summer 2023.",
+    },
+    "skills": {
+      "Languages": "Python, Java, C, C++, JavaScript, HTML, CSS, SQL",
+      "Frameworks": "React, Django, Flask, Bootstrap, Tailwind CSS",
+      "Tools": "Git, GitHub, VS Code, PyCharm, IntelliJ, Eclipse, Jupyter Notebook, MySQL Workbench, Postman, Figma, Adobe Photoshop, Adobe Illustrator, Adobe Premiere Pro, Adobe After Effects",
+    }, 
+  }
+
   return (
     <div className='flex'>
         <SideBar />
         <ChannelBar page="Experience" {...data}/>
-        <Content />
+        <Content {...content_data}/>
     </div>
   )
 }
